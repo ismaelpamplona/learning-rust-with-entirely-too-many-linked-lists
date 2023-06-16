ca9cd2e3ce9817661dd9f27ce8189911ea861f4a
Ismael Pamplona <ismaelpamplona@hotmail.com>
2023-06-16 07:01:26 -0300

    t2_2_new/

---
+++ t2_2_new/src/lib.rs
@@ -0,0 +1,31 @@
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+}
+
+pub fn box_list() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_01() {
+        let result = box_list();
+    }
+}
